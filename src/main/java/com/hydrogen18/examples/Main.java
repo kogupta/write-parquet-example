21a045b50320cd8b8a96090986b927502319332a
Kohinoor Gupta <kohinoor.gupta@gmail.com>
2021-08-25 15:26:29 +0530

    misc

--- src/main/java/com/hydrogen18/examples/Main.java
+++ src/main/java/com/hydrogen18/examples/Main.java
@@ -16,35 +16,35 @@ import org.apache.parquet.avro.AvroSchemaConverter;
 import org.apache.parquet.avro.AvroParquetWriter;
 
 public final class Main {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     Schema avroSchema = UserRank.getClassSchema();
-    MessageType parquetSchema = new AvroSchemaConverter().convert(avroSchema);    
-    
-    UserRank dataToWrite[] = new UserRank[]{
-      new UserRank(1, 3),
-      new UserRank(2, 0),
-      new UserRank(3, 100)
+    MessageType parquetSchema = new AvroSchemaConverter().convert(avroSchema);
+
+    UserRank[] dataToWrite = new UserRank[]{
+        new UserRank(1, 3),
+        new UserRank(2, 0),
+        new UserRank(3, 100)
     };
 
-  
+
     Path filePath = new Path("./example.parquet");
     int blockSize = 1024;
     int pageSize = 65535;
-    try(
-      AvroParquetWriter parquetWriter = new AvroParquetWriter(
-        filePath, 
-        avroSchema, 
-        CompressionCodecName.SNAPPY, 
-        blockSize, 
-        pageSize)
-      ){
-      for(UserRank obj : dataToWrite){
-        parquetWriter.write(obj); 
+    try (
+        AvroParquetWriter parquetWriter = new AvroParquetWriter(
+            filePath,
+            avroSchema,
+            CompressionCodecName.SNAPPY,
+            blockSize,
+            pageSize)
+    ) {
+      for (UserRank obj : dataToWrite) {
+        parquetWriter.write(obj);
       }
-    }catch(java.io.IOException e){
+    } catch (java.io.IOException e) {
       System.out.println(String.format("Error writing parquet file %s", e.getMessage()));
       e.printStackTrace();
     }
-      
+
   }
 }
